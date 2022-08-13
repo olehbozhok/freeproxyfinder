0b9af2613709d4d89d7ed60495a833f26836ff9b
Oleh Bohzok <olehbozhok@gmail.com>
2022-08-13 03:01:47 +0300

    update to get 500 proxy

--- parsers/parser_spys_one.go
+++ parsers/parser_spys_one.go
@@ -2,11 +2,14 @@ package parsers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/robertkrimen/otto"
@@ -17,39 +20,72 @@ var (
 	reIP       = regexp.MustCompile(`\d+.\d+.\d+.\d+\:\d+`)
 )
 
+var client http.Client
+
+func init() {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatalf("Got error while creating cookie jar %s", err.Error())
+	}
+
+	client = http.Client{
+		Jar:     jar,
+		Timeout: time.Second * 30,
+	}
+}
+
 // GetProxiesListSpysOne get proxy list from http://spys.one/en/socks-proxy-list/
 func GetProxiesListSpysOne(countryISO string) (proxylist []ProxySocks5Conf, err error) {
-	form := url.Values{}
-	form.Add("xf1", "0")
-	form.Add("xf2", "0")
-	form.Add("xf4", "0")
-	// socks5
-	form.Add("xf5", "2")
-	// 500 on page
-	form.Add("xpp", "5")
-	formData := form.Encode()
+	return getProxiesListSpysOneInternal("", countryISO)
+}
+
+func getProxiesListSpysOneInternal(xx0, countryISO string) (proxylist []ProxySocks5Conf, err error) {
+	fmt.Println("xx0:", xx0)
 
 	var req *http.Request
+
+	URL := ""
 	if countryISO == "" || countryISO == "*" {
-		req, err = http.NewRequest("POST", "http://spys.one/en/socks-proxy-list/", strings.NewReader(formData))
+		URL = "https://spys.one/en/socks-proxy-list/"
 	} else {
-		req, err = http.NewRequest("POST", fmt.Sprintf("http://spys.one/free-proxy-list/%s/", countryISO), strings.NewReader(formData))
+		URL = fmt.Sprintf("https://spys.one/free-proxy-list/%s/", countryISO)
+	}
+
+	var header http.Header = map[string][]string{
+		"User-Agent":      {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36"},
+		"Accept":          {"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"},
+		"Accept-Language": {"ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3"},
+		"Host":            {"spys.one"},
+		"Referer":         {"http://spys.one/en/"},
+		"origin":          {"https://spys.one"},
+	}
+
+	if xx0 == "" {
+		req, err = http.NewRequest("GET", URL, nil)
+	} else {
+		form := url.Values{}
+		form.Add("xx0", xx0)
+		// 500 on page
+		form.Add("xpp", "5")
+		form.Add("xf1", "0")
+		form.Add("xf2", "0")
+		form.Add("xf4", "0")
+		// socks5
+		form.Add("xf5", "2")
+		formData := form.Encode()
+		req, err = http.NewRequest("POST", URL, strings.NewReader(formData))
+
+		header.Add("Content-Type", "application/x-www-form-urlencoded")
+		header.Add("Content-Length", strconv.Itoa(len(formData)))
+
 	}
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header = map[string][]string{
-		"User-Agent":      {"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"},
-		"Accept":          {"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"},
-		"Accept-Language": {"ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3"},
-		"Host":            {"spys.one"},
-		"Referer":         {"http://spys.one/en/"},
-		"Content-Type":    {"application/x-www-form-urlencoded"},
-	}
-	req.Header.Add("Content-Length", strconv.Itoa(len(formData)))
+	req.Header = header
 
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -59,6 +95,17 @@ func GetProxiesListSpysOne(countryISO string) (proxylist []ProxySocks5Conf, err 
 	if err != nil {
 		return nil, err
 	}
+	for _, c := range client.Jar.Cookies(req.URL) {
+		fmt.Println(c.Name, c.Value)
+	}
+
+	if xx0 == "" {
+		xx0_1, ok := doc.Find(`body > table:nth-child(3) > tbody > tr:nth-child(4) > td > table > tbody > tr:nth-child(1) > td:nth-child(3) > input[type=hidden]`).Attr("value")
+		if ok && xx0_1 != "" {
+			return getProxiesListSpysOneInternal(xx0_1, countryISO)
+		}
+	}
+
 	scriptText := doc.Find(`html body script`).Nodes[2].FirstChild.Data
 
 	vm := otto.New()
